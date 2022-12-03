efe961ff59a0063aee4e06b9e8bf7dc360d52166
Aditya Kanala <kanala.a@northeastern.edu>
2022-12-03 18:03:36 -0500

    Added Select Statements

---
+++ SelectStatements.sql
@@ -0,0 +1,14 @@
+use aed_project;
+
+select * from admins;
+select * from csr;
+select * from customer;
+select * from delivery_agent;
+select * from farms;
+select * from shipping_agent;
+select * from ssr;
+select * from store;
+select * from store_manager;
+select * from supplier;
+select * from warehouse;
+select * from warehouse_manager;
