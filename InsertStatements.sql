6438f158d7c04a1cde5294d6d8cee311a39a9b2f
Mahith ‎ <mahith@Mahiths-MacBook-Pro.local>
2022-12-09 16:57:03 -0500

    folders organised

--- InsertStatements.sql
+++
@@ -1,8 +0,0 @@
-/*Use Daabase*/
-use aed_project;
-
-/*Admin Table*/
-INSERT INTO `aed_project`.`admins`
-(`UserName`, `Password`)
-VALUES
-('sadmin', 'sadmin'),('mpadmin','mpadmin'),('bpoadmin','bpoadmin'),('ladmin','ladmin'),('dadmin','dadmin');
